68ff94c819bb7780a673d30269b5f5ca93813283
sefthus <elisabeth.strom95@gmail.com>
2018-02-15 15:56:20 +0100

    started report

--- src/mlst1_plot.py
+++ src/mlst1_plot.py
@@ -3,32 +3,57 @@ import matplotlib as mpl
 mpl.use('TkAgg') # Ensure that the Tkinter backend is used for generating figures
 import matplotlib.pyplot as plt
 
-Mpc = 3.08568025e22
 
+#------------------------------------- reading in data
 omega_m, omega_b, omega_r, omega_lambda = np.loadtxt("omega_mbrl.dat",usecols=(0,1,2,3), unpack=True)
-x_eta, eta = np.loadtxt("xeta_eta.dat",usecols=(0,1), unpack=True)
-x_t, eta_t = np.loadtxt("xt_eta_t.dat",usecols=(0,1), unpack=True)
-z, H = np.loadtxt("xeta_z_H.dat",usecols=(1,2), unpack=True)
+x_eta, eta  	= np.loadtxt("xeta_eta.dat",usecols=(0,1), unpack=True)
+z, H 			= np.loadtxt("xeta_z_H.dat",usecols=(1,2), unpack=True)
 
+
+#------------------------------------- defining stuff
+Mpc = 3.08568025e22
+omega_total = omega_m+omega_b+omega_r+ omega_lambda
 eta = eta/Mpc # m --> Mpc units
-eta_t = eta_t/Mpc
-H = H* Mpc/1e2 # s^-1 --> km s^-1/Mpc
+H = H* Mpc/1e3 # s^-1 --> km s^-1/Mpc
 
+
+#------------------------------------- calculating radiation matter dark energy equality
+indx1 = [i for i in range(len(omega_m)) if omega_r[i]>=omega_m[i]][-1]
+x_radmat_eq = x_eta[indx1]
+a_radmat_eq = np.exp(x_radmat_eq)
+z_radmat_eq = 1./a_radmat_eq -1
+omega_radmat_eq = omega_r[indx1]
+
+indx2 = [i for i in range(len(omega_m)) if omega_m[i]>=omega_lambda[i]][-1]
+x_matlam_eq = x_eta[indx2]
+a_matlam_eq = np.exp(x_matlam_eq)
+z_matlam_eq = 1./a_matlam_eq -1
+omega_matlam_eq = omega_m[indx2]
+
+print 'radiation - matter equality at:   x = %.3f , a = %.4f , z = %.3f, Omega = %.3f' % (x_radmat_eq, a_radmat_eq, z_radmat_eq, omega_radmat_eq)
+print 'dark energy - matter equality at: x = %.3f , a = %.4f , z = %.3f, Omega = %.3f' % (x_matlam_eq, a_matlam_eq, a_matlam_eq, omega_matlam_eq)
+
+#------------------------------------- plotting
 plt.rc('text',usetex=True)
 plt.rc('font',**{'family':'serif','size':14})
 
+#------------------------------------- plotting omega
 plt.figure()
 plt.plot( x_eta, omega_m, label = r'$\Omega_\mathrm{m}$')
 plt.plot( x_eta, omega_b, label = r'$\Omega_\mathrm{b}$')
 plt.plot( x_eta, omega_r, label = r'$\Omega_\mathrm{r}$')
-plt.plot( x_eta, omega_lambda, label= '$\Omega_\Lambda$')
-plt.plot( x_eta, omega_m+omega_b+omega_r+ omega_lambda, label= '$\Omega_\mathrm{Total}$')
+plt.plot( x_eta, omega_lambda, label= r'$\Omega_\Lambda$')
+plt.plot( x_eta, omega_total, label= r'$\Omega_\mathrm{Total}$')
+
+plt.plot( x_radmat_eq, omega_radmat_eq, 'x', label = r'$\Omega_{r=m}$')
+plt.plot( x_matlam_eq, omega_matlam_eq, 'x', label = r'$\Omega_{m=\Lambda}$')
 plt.xlabel(r'$x$')
 plt.ylabel(r'$\Omega_\mathrm{X}$')
 plt.legend(loc='center left')
 plt.grid('on', linestyle ='--')
-#plt.show()
-
+plt.show()
+'''
+#------------------------------------- plotting H against x
 plt.figure()
 plt.plot(x_eta,H)
 plt.xlabel(r'$x$')
@@ -37,7 +62,7 @@ plt.yscale('log')
 plt.grid('on', linestyle ='--')
 
 #plt.show()
-
+#------------------------------------- plotting H against z
 plt.figure()
 plt.plot(z,H)
 plt.gca().invert_xaxis() # because z
@@ -47,11 +72,10 @@ plt.yscale('log')
 plt.xscale('log')
 plt.grid('on', linestyle ='--')
 plt.show()
-
+'''
+#------------------------------------- plotting eta against x
 plt.figure()
-plt.plot(x_eta,eta, label=r'$\eta_{computed}$')
-plt.plot(x_t,eta_t,'--', label=r'$\eta_{splined}$')
-plt.legend(loc='lower right')
+plt.plot(x_eta,eta)
 plt.yscale('log')
 plt.xlabel(r'$x$')
 plt.ylabel(r'$\eta(x)$ [Mpc]')
