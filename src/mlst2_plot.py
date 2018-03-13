30d89dd7a22a20593f738c902898f022f29211d6
sefthus <elisabeth.strom95@gmail.com>
2018-03-13 22:07:27 +0100

    plots may be in order

---
+++ src/mlst2_plot.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib as mpl
+mpl.use('TkAgg') # Ensure that the Tkinter backend is used for generating figures
+import matplotlib.pyplot as plt
+
+#--------------------------- reading in data
+tau, dtau, ddtau		= np.loadtxt("x_tau.dat",usecols=(0,1,2), unpack=True)
+g, dg, ddg  			= np.loadtxt("x_g.dat",usecols=(0,1,2), unpack=True)
+x, z, Xe 				= np.loadtxt("z_Xe.dat",usecols=(0,1,2), unpack=True)
+
+#----------------------------- defining stuffu
+Mpc = 3.08568025e22
+
+
+#------------------------------ plotting
+plt.rc('text',usetex=True)
+plt.rc('font',**{'family':'serif','size':16})
+
+#-------------------------------- plotting tau and derivatives
+plt.figure()
+plt.plot( x, tau, label = r'$\tau(x)$')
+plt.plot( x, np.abs(dtau), label = r"$|\tau'|$")
+plt.plot( x, np.abs(ddtau), label = r"$|\tau''|$")
+plt.xlabel(r'$x$')
+plt.ylabel(r"$\tau(x),\,|\tau'(x)|,\,|\tau''(x)|$")
+plt.legend(loc='center left')
+plt.yscale('log')
+plt.grid('on', linestyle ='--')
+plt.show()
+
+
+#--------------------------------- plotting g and derivatives
+plt.figure()
+plt.plot( x, g, label = r'$\tilde{g}(x)$')
+plt.plot( x, dg/10., label = r"$\tilde{g}'(x)$")
+plt.plot( x, ddg/300., label = r"$\tilde{g}''(x)$")
+plt.xlabel(r'$x$')
+plt.ylabel(r"$ \tilde{g}(x),\,\tilde{g}(x)'/10,\,\tilde{g}(x)''/300$")
+#plt.yscale('log')
+plt.legend(loc='best')
+plt.grid('on', linestyle ='--')
+#plt.show()
+
+plt.show()
+'''
+#----------------------------------- plotting X_e against z
+plt.figure()
+plt.plot(z,Xe)
+plt.gca().invert_xaxis() # because z
+plt.xlabel(r'$z$')
+plt.ylabel(r'$X_\mathrm{e}(z)$')
+plt.yscale('log')
+plt.xlim([2000,0])
+plt.ylim([1e-4,1.4])
+plt.grid('on', linestyle ='--')
+plt.show()
+'''
+
